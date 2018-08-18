69a59150ac7f76d3583715fcb6e51782f318f8d4
639dev <gharood_3@hotmail.com>
2018-08-18 19:03:01 +0300

    design question card

--- src/containers/AllQuestions.js
+++ src/containers/AllQuestions.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import '../assets/css/AllQuestions.css' 
 import {Tabs} from '../components/Tabs'
 import {Tab} from '../components/Tab'
+import { Button, Card, Elevation } from "@blueprintjs/core";
+import score from '../assets/images/score.png';
 
 
 class AllQuestions extends React.Component {
@@ -12,7 +14,19 @@ class AllQuestions extends React.Component {
 			<div className="container">
 				<Tabs>
 				    <Tab title="Unanswered Questions" linkClassName={'link-class-0'} className="tab">
-				        <p>content 0</p>
+				        <Card className="card">
+						    <h2><a href="#">Writer asks...</a></h2>
+						    <div className="card-info">
+						    	<div className="image-div">
+						    		<img src={score} alt="user-img" className="card-img" />
+						    	</div>
+						    	<div className="q-div">
+						    		<p> would you rather ... </p>
+
+						    		<button> View Poll </button>
+						    	</div>
+						    </div>
+						</Card>
 				    </Tab>
 				    <Tab title="Answered Questions" linkClassName={'link-class-1'} className="tab">
 				       <h1>content 1</h1>
